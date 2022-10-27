4f6c72514de64e169692d74d9e4d439270159b6d
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-10-28 00:13:32 +0300

    hw7 - 01

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,7 +35,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+        onChange?.(e)
+        onChangeOption?.(e.currentTarget.checked) // делают студенты
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,6 +49,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                      name={name}
+                      // checked={o.id === 1}
+                     value={o.value}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
