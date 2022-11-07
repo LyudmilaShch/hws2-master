5dfcf8056e2b4dc7baab493685af9771a56515de
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2022-11-08 01:50:29 +0300

    hw8 - 01 without css

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,27 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+
+export const homeWorkReducer = (people: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            switch (action.payload) {
+                case "up": {
+                    return people.sort((a, b) => a.name.localeCompare(b.name))
+                }
+                case "down": {
+                    return people.sort((a, b) => b.name.localeCompare(a.name))
+                }
+                default:
+                    return people
+            }
+            // need to fix
         }
         case 'check': {
+            return people.filter((el) => el.age >= action.payload)}
+            // need to fix
 
-            return state // need to fix
-        }
         default:
-            return state
+            return people
+        }
     }
-}
